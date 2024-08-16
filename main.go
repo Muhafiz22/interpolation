0985a3111591221251ed03d80c1a120015422218
muhafiz <hussainmuhafiz@gmail.com>
2024-08-16 21:50:02 +0530

    Complete forward diffn interpolation

--- main.go
+++ main.go
@@ -8,17 +8,61 @@ import (
 func main() {
 
 }
-func validateInput(x []float32, y []float32) (error, []float32, []float32) {
+func validateInput(x []float64, y []float64) ([]float64, []float64, error) {
 
 	if len(x) != len(y) {
-		return errors.New("Insufficient Input"), nil, nil
+		return nil, nil, errors.New("insufficient Input")
 	}
-	return nil, x, y
+	return x, y, nil
 
 }
 
-func checkForward(x []float32, xu float32) bool {
+func checkForward(x []float64, xu float64) bool {
 
 	return xu <= x[int(math.Abs(float64(len(x)/2)))]
 
+}
+
+func calculateForwardDiffInterpolation(x []float64, y []float64, xu float64) float64 {
+
+	n := len(x)
+	delta_Y := make([][]float64, n)
+
+	for i := range delta_Y {
+
+		delta_Y[i] = make([]float64, n-i) //Creating rows corresponding to different coloumn sizes
+		delta_Y[i][0] = y[i]
+	}
+
+	for j := 1; j < n; j++ { //iterating for diffn degree frwrd differences
+		for i := 0; i < n-j; i++ { // iterating no.of row values to be calculated for diff frwrd diff
+
+			delta_Y[i][j] = delta_Y[i+1][j-1] - delta_Y[i][j-1] //Calculating higher frwd diffn
+		}
+	}
+
+	result := y[0]
+	u := (xu - x[0]) / (x[1] - x[0])
+	uproduct := 1.0
+
+	for i := 1; i < n; i++ {
+
+		uproduct *= (u - float64(i-1))
+		result += (uproduct * delta_Y[0][i]) / factorial(i)
+	}
+
+	return result
+}
+
+func factorial(n int) float64 {
+
+	fact := 1.0
+	if n == 0 || n == 1 {
+		return 1
+	}
+	for i := 2; i <= n; i++ {
+
+		fact *= float64(i)
+	}
+	return fact
 }
--- main_test.go
+++ main_test.go
@@ -7,28 +7,42 @@ import (
 
 func TestMain(t *testing.T) {
 
-	r := make([]float32, 5)
-	s := make([]float32, 5)
-	s = append(s, 20, 30, 40, 50, 60)
-	r = append(r, 2, 3, 4, 5, 6)
+	r := []float64{40, 50, 60, 70, 80}
+
+	s := []float64{31.0, 73.0, 124.0, 159.0, 190.0}
 	err, _, _ := validateInput(r, s)
 
 	if err != nil {
-		t.Fatal(err.Error())
+		fmt.Println(err)
 	}
 
 }
 
 func TestCheckForward(t *testing.T) {
 
-	r := make([]float32, 5)
-	r = append(r, 2, 3, 4, 5, 6)
-	var p float32 = 1.5
+	r := []float64{40, 50, 60, 70, 80}
+
+	var p float64 = 45.0
 	c := checkForward(r, p)
 
 	if c {
-		fmt.Print("use forward")
+		fmt.Println("use forward")
 	} else {
-		fmt.Print("use backward")
+		fmt.Println("use backward")
+	}
+}
+
+func TestCalculateForwardDiffInterpolation(t *testing.T) {
+
+	x := []float64{40, 50, 60, 70, 80}
+
+	y := []float64{31.0, 73.0, 124.0, 159.0, 190.0}
+	xu := 45.0
+	d := calculateForwardDiffInterpolation(x, y, xu)
+	fmt.Println(d)
+	if d == 15.468750000000002 {
+		fmt.Println("Test passed")
+	} else {
+		fmt.Println("Test failed")
 	}
 }
